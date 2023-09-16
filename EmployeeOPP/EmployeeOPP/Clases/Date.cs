805fb1dfebbff06b52a27e0134ad33a33e0734fd
Leidy Melissa Trejos Pamplona <leidy.meli14@gmail.com>
2023-09-16 18:34:12 -0500

    Employee class create

--- EmployeeOPP/EmployeeOPP/Clases/Date.cs
+++ EmployeeOPP/EmployeeOPP/Clases/Date.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+
+using EmployeeOPP.Clases;
 
 namespace EmployeeOPP.Clases
 {
@@ -20,9 +23,79 @@ namespace EmployeeOPP.Clases
         #region Methods
         public Date(int day, int month,int year)
         {
-            _year = year;
-            _month = month;
-            _day = day;
+            _year = validateYear(year);
+            _month = validateMonth(month);
+            _day = validateDay(day, month, year);
+        }
+
+        private int validateDay(int day, int month, int year)
+        {
+            //valido si ek dia ingresado pertenece a un año bisiesto
+            if(month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+            
+            if(month == 2 && day == 29 && IsLeapYear(year))
+            {
+                bool isLeapYear = false;
+                IsLeapYearException(isLeapYear, year);
+            }
+
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+            else
+            {
+                //Exeception creation
+                throw new DayException(String.Format("El dia {0} no es valido para el mes {1}", day, month));
+            }
+        }
+
+        private void IsLeapYearException(bool isLeapYear, int year)
+        {
+            if (!isLeapYear)
+            {
+                //Exeception creation
+                throw new YearException(String.Format("El año {0} es biciesto", year));
+            }
+            
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 !=0;
+        }
+
+        //validate year
+        private int validateYear(int year)
+        {
+            if (year >= 1900)
+            {
+                return year;
+            }
+            else
+            {
+                //Exeception creation
+                throw new YearException(String.Format("El año {0} no es válido", year));
+            }
+        }
+
+        //Validar mes
+        private int validateMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+            else
+            {
+                //Exeception creation, las exepciones siempre se lanzan
+                throw new MonthException(String.Format("El mes {0} no es válido", month));
+            }
         }
 
         public override string ToString() {
@@ -32,15 +105,35 @@ namespace EmployeeOPP.Clases
             //string dateConcatenated = _day + "/" + _month + "/" + _year;
 
             //Concatenacion en interpolacion
-            //string dateConcatenated = $"{_day}/{_month}/{_year}";
+            //string dateConcatenated = $"{_day:00}/{_month:00}/{_year:0000}";
 
             //Concatenacion con la clase String.Format
-            string dateConcatenated = String.Format("{0}/{1}/{2}", _day,_month,_year);
+            string dateConcatenated = String.Format("{0:00}/{1:00}/{2:00}", _day,_month,_year);
             
             //retorno
             return dateConcatenated;
         }
 
         #endregion
+    }
+
+    [Serializable]
+    internal class DayException : Exception
+    {
+        public DayException()
+        {
+        }
+
+        public DayException(string? message) : base(message)
+        {
+        }
+
+        public DayException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected DayException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
--- EmployeeOPP/EmployeeOPP/Program.cs
+++ EmployeeOPP/EmployeeOPP/Program.cs
@@ -1,10 +1,33 @@
 ﻿// Referenciar y usar
 using EmployeeOPP.Clases;
 
-Console.WriteLine("OPP Aplication");
-Console.WriteLine("--------------");
+try
+{
+    Console.WriteLine("OPP Aplication");
+    Console.WriteLine("--------------");
 
-//Inicializacion de la clase date
-//crear un objeto a partir de la clase
-Date dateObject= new Date(4,5,2023);
-Console.WriteLine(dateObject.ToString());
+    int day, month, year;
+
+    Console.Write("Ingresar el díá: ");
+    day = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el mes: ");
+    month = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el año: ");
+    year = Convert.ToInt32(Console.ReadLine());
+
+
+    //Inicializacion de la clase date
+
+    //crear un objeto a partir de la clase
+    Date dateObject = new Date(day, month, year);
+    Console.WriteLine("\n");
+    Console.WriteLine(dateObject.ToString());
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
+
